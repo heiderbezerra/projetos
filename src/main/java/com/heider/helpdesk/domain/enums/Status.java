036e4d8841b531512a5ff71710e4594c932289be
heiderbezerra <heiderbezerra18@gmail.com>
2024-06-11 08:25:37 -0300

    Tipo Enum

---
+++ src/main/java/com/heider/helpdesk/domain/enums/Status.java
@@ -0,0 +1,38 @@
+package com.heider.helpdesk.domain.enums;
+
+public enum Status {
+
+	ABERTO (0, "ROLE_ABERTO"),
+	ANDAMENTO (1, "ROLE_ANDAMENTO"),
+	ENCERRADO (2, "ROLE_ENCERRADO");
+	
+	private Integer codigo;
+	private String descricao;
+	
+	private Status(Integer codigo, String descricao) {
+		this.codigo = codigo;
+		this.descricao = descricao;
+	}
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+	
+
+	public static Status toEnum(Integer cod) {
+		if (cod == null) {
+			return null;
+		}
+		for (Status status : Status.values()) {
+			if (cod.equals(status.codigo)) {
+				return status;
+			}
+		}
+		throw new IllegalArgumentException("Status inválido");
+	}
+	
+}
